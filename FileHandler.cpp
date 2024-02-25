4744786c870c715074d2b93189e3f51ebc1d1ab9
Ivan Damyanov <damyanov.dimitrov.ivan@gmail.com>
2024-02-25 19:55:55 +0200

    Fixed previous deletion of code and modified the logic of saving and loading game score for multiple games

--- FileHandler.cpp
+++ FileHandler.cpp
@@ -2,9 +2,22 @@
 #include <fstream>
 
 void FileHandler::SaveScore(int score) {
-    std::ofstream file("score.txt");
+    std::ofstream file("score.txt", std::ios_base::app);
     if (file.is_open()) {
-        file << score;
+        file << score << std::endl;
         file.close();
     }
+}
+
+std::vector<int> FileHandler::LoadScores() {
+    std::vector<int> scores;
+    std::ifstream file("score.txt");
+    int score;
+    if (file.is_open()) {
+        while (file >> score) {
+            scores.push_back(score);
+        }
+        file.close();
+    }
+    return scores;
 }
--- FileHandler.h
+++ FileHandler.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <vector>
+
 class FileHandler {
 public:
     void SaveScore(int score);
+    std::vector<int> LoadScores();
 };
