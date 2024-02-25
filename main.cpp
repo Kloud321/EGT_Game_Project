75f470253494b5715aa3cf9fd0e93c20df4835b7
Ivan Damyanov <damyanov.dimitrov.ivan@gmail.com>
2024-02-25 21:38:38 +0200

    Disable window resize func to maintain fixed dimnesions

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
     Game game;
 
     if (!game.Init("Brick Breaker", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE)) {
+        WINDOW_WIDTH, WINDOW_HEIGHT, 0)) {
         std::cerr << "Failed to initialize game!" << std::endl;
         return 1;
     }
